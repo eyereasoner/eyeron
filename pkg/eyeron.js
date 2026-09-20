1f9df91867b6a5ca6518400798d3cbc762a283ea
josd <josderoo@gmail.com>
2026-09-20 22:07:31 +0200

    Extend 5 more example groups to all three formats; 120 of 134 now have full parity

--- pkg/eyeron.js
+++ pkg/eyeron.js
@@ -359,7 +359,7 @@ function __wbg_get_imports() {
         __wbg___wbindgen_throw_344f42d3211c4765: function(arg0, arg1) {
             throw new Error(getStringFromWasm0(arg0, arg1));
         },
-        __wbg_now_563651e0cf1289d8: function() {
+        __wbg_now_46c96d110ab71c75: function() {
             const ret = Date.now();
             return ret;
         },
