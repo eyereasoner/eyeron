9e48c45a60be8e0b9eddf4d525428432d366fdab
josd <josderoo@gmail.com>
2026-09-20 23:50:28 +0200

    Add N3 sudoku:solve builtin, drop relational-cube-lookup; full N3/eye/SRL parity

--- pkg/eyeron.js
+++ pkg/eyeron.js
@@ -359,7 +359,7 @@ function __wbg_get_imports() {
         __wbg___wbindgen_throw_344f42d3211c4765: function(arg0, arg1) {
             throw new Error(getStringFromWasm0(arg0, arg1));
         },
-        __wbg_now_1b408424a43ee0a5: function() {
+        __wbg_now_ee33295a7a6fe132: function() {
             const ret = Date.now();
             return ret;
         },
