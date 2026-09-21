d26904239adb108ffe28413d0e99af102a1c3177
josd <josderoo@gmail.com>
2026-09-22 01:08:58 +0200

    Make Eyelang homoiconic with clause/2 and prove/1

--- pkg/eyeron.js
+++ pkg/eyeron.js
@@ -442,7 +442,7 @@ function __wbg_get_imports() {
         __wbg___wbindgen_throw_344f42d3211c4765: function(arg0, arg1) {
             throw new Error(getStringFromWasm0(arg0, arg1));
         },
-        __wbg_now_c4eae40a4f3cf289: function() {
+        __wbg_now_f68b49ad084b9858: function() {
             const ret = Date.now();
             return ret;
         },
