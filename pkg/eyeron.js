204ae2b16876ce01137fd198e80f559f7239bd51
josd <josderoo@gmail.com>
2026-09-21 11:14:08 +0200

    N3/SRL printer blank-line paragraphing; cosmetic example formatting

--- pkg/eyeron.js
+++ pkg/eyeron.js
@@ -359,7 +359,7 @@ function __wbg_get_imports() {
         __wbg___wbindgen_throw_344f42d3211c4765: function(arg0, arg1) {
             throw new Error(getStringFromWasm0(arg0, arg1));
         },
-        __wbg_now_ee33295a7a6fe132: function() {
+        __wbg_now_fa8e05bf6d31c159: function() {
             const ret = Date.now();
             return ret;
         },
